569b2276a89d83e6410cb80ac05612cc4d5a2a8d
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-04-28 15:27:50 +0530

    contact page css

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -18,6 +18,7 @@ const Contact = () => {
             <div className="text-zone">
                 <h1>
                     <AnimatedLetters 
+                        letterClass={letterClass}
                         strArray={[
                                     'C',
                                     'o',
@@ -33,6 +34,49 @@ const Contact = () => {
                         idx={15}
                     />
                 </h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, a illo reiciendis sequi nobis eligendi debitis porro? Iste totam ad dolorem eaque, commodi unde qui officia odio quibusdam consectetur aliquid.</p>
+                <div className='contact-form'>
+                    <form>
+                        <ul>
+                            <li className='haft'>
+                                <input 
+                                    type='text' 
+                                    name='name'
+                                    placeholder='Name'
+                                    required
+                                     />
+                            </li>
+                            <li className='haft'>
+                                <input 
+                                    type='email'
+                                    name='email'
+                                    placeholder='Email'
+                                    required />
+                            </li>
+                            <li> 
+                                <input 
+                                    name='subject'
+                                    placeholder='Subject'
+                                    type='text'
+                                    required />
+                            </li>
+                            <li>
+                                <textarea 
+                                    placeholder='Message'
+                                    name='message'
+                                    required
+                                ></textarea>
+                            </li>
+                            <li>
+                                <input
+                                    type='submit' 
+                                    className='flat-button'
+                                    value='SEND'
+                                />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
         </div>
         <Loader type='pacman' />
