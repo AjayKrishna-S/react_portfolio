ee863bbf43ec8ed5eb55ca83d145dd17e928631a
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-05-03 19:10:40 +0530

    map section creation & animation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,38 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
 
     useEffect(() => {
         setTimeout(()=>{
             setLetterClass('text-animate-hover')
         },3000)
     },[])
+
+    const sendEmail = (e) => {
+        emailjs
+            .sendForm(
+                'gmail',
+                'templete',
+                refForm.current,
+                // 'user_token_email'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again') 
+                }
+            )
+    }
 
   return (
     <>
@@ -36,7 +58,7 @@ const Contact = () => {
                 </h1>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, a illo reiciendis sequi nobis eligendi debitis porro? Iste totam ad dolorem eaque, commodi unde qui officia odio quibusdam consectetur aliquid.</p>
                 <div className='contact-form'>
-                    <form>
+                    <form ref={refForm} onSubmit={sendEmail}>
                         <ul>
                             <li className='half'>
                                 <input 
@@ -77,8 +99,25 @@ const Contact = () => {
                         </ul>
                     </form>
                 </div>
+
+            </div>
+            <div className='info-map'>
+                S. Ajay Krishna, <br />
+                Thisayanvilai (tk), <br />
+                Tirunelveli (dt), <br />
+                Tamil Nadu - 627657 <br />
+                <span>ajaykrishna.sak@gmail.com</span>
+            </div>
+            <div className="map-wrap">
+                <MapContainer center={[8.307893911938974, 77.86042687696374]} zoom={13}>
+                    <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+                    <Marker position={[8.307893911938974, 77.86042687696374]} >
+                        <Popup>Ajay lives here, come over for a cup of coffee :)</Popup>
+                    </Marker>
+                </MapContainer>
             </div>
         </div>
+
         <Loader type='pacman' />
     </>
   )
