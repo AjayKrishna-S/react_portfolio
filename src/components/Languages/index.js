e0674e4f475dfed3fcdea09aae9a762a3da39b11
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-05-15 13:28:29 +0530

    set skill icons from firebase

--- src/components/Languages/index.js
+++ src/components/Languages/index.js
@@ -20,20 +20,20 @@ const Languages = () => {
         <div className='lan-html card'>
           <div className="card-border"></div>
           <div className="card-content">
-
+            <img src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-5a704.appspot.com/o/Skills%2Fhtml.png?alt=media&token=23f73a0a-a7f2-45c3-97d4-8055cabca073" alt="html" />
           </div>
             
         </div>
         <div className='lan-css card' > 
           <div className="card-border"></div>         
           <div className="card-content">
-
+            <img src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-5a704.appspot.com/o/Skills%2Fcss.png?alt=media&token=b95841ef-026b-4ee2-b7d0-fb28735c3a1b" alt="css" />
           </div>
         </div>
         <div className='lan-js card'>
           <div className="card-border"></div>
           <div className="card-content">
-
+            <img src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-5a704.appspot.com/o/Skills%2Fjs.png?alt=media&token=d7da2e40-50a7-4641-9309-5a08d2826dae" alt="js" />
           </div>
         </div>
       </div>
